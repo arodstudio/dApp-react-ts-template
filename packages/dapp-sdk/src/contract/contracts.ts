5aaeb6d1863329d4464b6f6143bdc3fbc4ae6346
Arod <arod.mail+fp@proton.me>
2023-12-31 17:14:54 -0300

    fix(sdk): remove lock instances

--- packages/dapp-sdk/src/contract/contracts.ts
+++ packages/dapp-sdk/src/contract/contracts.ts
@@ -1,13 +1,9 @@
-import { ERC20ABI, LockABI } from '@dapp/contracts'
+import { ERC20ABI } from '@dapp/contracts'
 import { getContractAddressesForChainOrThrow } from './addresses'
 
 export const getContractsDataForChainOrThrow = async (chainId: number) => {
   const addresses = await getContractAddressesForChainOrThrow(chainId)
   return {
-    Lock: {
-      abi: LockABI,
-      address: addresses.Lock,
-    },
     ERC20: {
       abi: ERC20ABI,
       address: addresses.ERC20Mock,
